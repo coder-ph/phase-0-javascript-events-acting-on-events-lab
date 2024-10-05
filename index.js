2b0ccaf1553ca9ae74126b6cf5e3b7a6fdb25c92
Phelix <phelixmbani@gmail.com>
2024-10-05 12:22:59 +0300

    events acting on events lab

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Your code here
+function moveDodgerLeft() {
+    const dodgerId = document.getElementById('dodger');
+    dodgerId.style.left = '0px';
+}
+moveDodgerLeft();
+
+function moveDodgerRight(){
+    const dodgerRight = document.getElementById('dodger');
+    dodgerRight.style.left = '370px';
+}
+moveDodgerRight();
